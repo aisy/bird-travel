d9c00f521f5a59d1dafc7dce087281afee002cb3
aisy <aisymuhammad@gmail.com>
2021-10-17 23:55:23 +0700

    change whole page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,22 @@
-import { Box, Container } from '@mui/material';
 import CarouselHome from '../components/CarouselHome';
-import Navbar from '../components/Navbar';
-import { ScrollTopButton, InitTopPosition } from '../components/ScrollTop';
+import DefaultLayout from '../layout/DefaultLayout';
 
-const HomePage = (props: any) => {
+const HomePage = () => {
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <Navbar />
-        <InitTopPosition />
-        <Container>
-          <Box sx={{ my: 2 }}>
-            <CarouselHome />
-            {[...new Array(50)].map(() =>
-              `Cras mattis consectetur purus sit amet fermentum.
+    <>
+      <DefaultLayout>
+        <CarouselHome />
+        <div style={{ marginInline: 60 }}>
+          {[...new Array(50)].map(() =>
+            `Cras mattis consectetur purus sit amet fermentum.
             Cras justo odio, dapibus ac facilisis in, egestas eget quam.
             Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
             Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
             `,
-            ).join('\n')}
-          </Box>
-        </Container>
-        <ScrollTopButton {...props} />
-      </Box>
-    </div>
+          ).join('\n')}
+        </div>
+      </DefaultLayout>
+    </>
   );
 }
 
