3a4d515ab7c7534f79b1704e9f7045798d448e18
aisy <aisymuhammad@gmail.com>
2021-10-17 23:53:58 +0700

    add: component CarousellHome

--- src/components/CarouselHome.tsx
+++ src/components/CarouselHome.tsx
@@ -5,29 +5,36 @@ const CarouselHome = () => {
   let items = [
     {
       name: "Random Name 1",
-      description: "Nani!?"
+      src: "https://images.unsplash.com/photo-1480044965905-02098d419e96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
     },
     {
       name: "Random Name 2",
-      description: "Nani!?"
+      src: "https://images.unsplash.com/photo-1485488001527-2e2259690bcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      name: "Random Name 3",
+      src: "https://images.unsplash.com/photo-1526367389455-42582383196c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
   ];
 
   return (
-    <Carousel>
+    <Carousel
+      animation={"fade"}
+      autoPlay={true}
+    >
       {
         items.map((item, i) => {
           return (
             <div key={i}>
-              {/* <Paper>
-                <h2>{item.name}</h2>
-                <p>{item.description}</p>
-
-                <Button className="CheckButton">
-                  Check it out!
-                </Button>
-              </Paper> */}
-              <img alt={"manuk"} src={"https://images.unsplash.com/photo-1480044965905-02098d419e96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"} />
+              <img
+                style={{
+                  width: "100%",
+                  height: 600,
+                  objectFit: "cover",
+                }}
+                alt={item.name}
+                src={item.src}
+              />
             </div>
           );
         })
