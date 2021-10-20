eebc03085480fb6cfa6c38081e0703d2fa4f6932
aisy <aisymuhammad@gmail.com>
2021-10-20 19:55:14 +0700

    add: new pages

---
+++ src/pages/TourDetailPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/system";
+
+
+const TourDetailPage = () => {
+  <Box>
+
+  </Box>
+}
+
+export default TourDetailPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // component pages
 import HomePage from '../pages/HomePage';
 import TourPage from '../pages/TourPage';
+import TourDetailPage from '../pages/TourDetailPage';
 
 const RouteApp: FunctionComponent<{}> = () => {
   return (
     <Router>
       <Switch>
         <Route path={"/"} exact component={HomePage} />
-        <Route path={"/tour"} component={TourPage} />
+        <Route path={"/tour"} exact component={TourPage} />
+        <Route path={"/tour/:id"} children={TourDetailPage} />
       </Switch>
     </Router>
   );
