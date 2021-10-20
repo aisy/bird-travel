94abf8c421aec76d8373018e44f532f1895c2432
aisy <aisymuhammad@gmail.com>
2021-10-21 00:25:24 +0700

    chage: content

--- src/pages/TourDetailPage.tsx
+++ src/pages/TourDetailPage.tsx
@@ -1,10 +1,20 @@
+import { Container } from "@mui/material";
 import { Box } from "@mui/system";
+import DefaultLayout from "../layout/DefaultLayout";
 
+const TourDetailPage = (props: any) => {
+  const dataTourDetail = props.location.state || {};
+  // let { id } = useParams;
 
-const TourDetailPage = () => {
-  <Box>
-
-  </Box>
+  return (
+    <DefaultLayout>
+      <Box>
+        <Container>
+          {dataTourDetail.title}
+        </Container>
+      </Box>
+    </DefaultLayout>
+  );
 }
 
 export default TourDetailPage;
