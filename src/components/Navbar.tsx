b4d1d87d9866c8ecd2f0a695b5448345a970f738
aisy <aisymuhammad@gmail.com>
2021-10-21 17:27:25 +0700

    add: link and remove unused lib

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
-import { AppBar, Toolbar, Typography, Link, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography, Link } from '@mui/material';
+// import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
   return (
     <AppBar position="fixed" sx={{ paddingLeft: 30, paddingRight: 30 }}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Kunam Tours
+          <Link
+            underline={"none"}
+            color={"inherit"}
+            href={'/'}
+          >
+            Kunam Tours
+          </Link>
         </Typography>
         <nav>
           <Link
