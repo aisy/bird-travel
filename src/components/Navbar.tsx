252a43e15f5446d6be7c2cd4390c00da058da78c
aisy <aisymuhammad@gmail.com>
2021-11-06 22:05:46 +0700

    add button in mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
   const [changeNavbarColor, setChangeNavbarColor] = useState(false);
@@ -13,14 +14,12 @@ const Navbar = () => {
   }
 
   useEffect(() => {
-    return () => {
-      changeBackgroundColor();
-      window.addEventListener("scroll", changeBackgroundColor)
-    };
+    changeBackgroundColor();
+    window.addEventListener("scroll", changeBackgroundColor)
   });
 
   return (
-    <div className={`navbar mb-2 fixed w-full text-black z-50 ${changeNavbarColor ? 'shadow-lg bg-white' : 'bg-transparent'} `}>
+    <div className={`navbar mb-2 fixed w-full text-black z-50 ${changeNavbarColor ? 'shadow-lg bg-white' : 'bg-transparent '}`}>
       <div className={"flex-1 px-2 mx-2"}>
         <span className={"text-lg font-bold"}>
           Manuk
@@ -44,9 +43,7 @@ const Navbar = () => {
       </div>
       <div className="lg:hidden sm:flex-none">
         <button className="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
+          <FiMenu />
         </button>
       </div>
     </div>
